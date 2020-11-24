bdcb858df936d2a5dbd6f1486e51af5ef1005c46
takuma0531 <tb.masaki0531@gmail.com>
2020-11-24 15:45:38 +0200

    apply databinding to title fragment

--- app/src/main/java/com/example/customfortune/uicontrollers/TitleFragment.kt
+++ app/src/main/java/com/example/customfortune/uicontrollers/TitleFragment.kt
@@ -5,14 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.example.customfortune.R
+import com.example.customfortune.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
+    private lateinit var binding: FragmentTitleBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_title, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container,false)
+
+        return binding.root
     }
 }
