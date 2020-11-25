02b2d2f4625a2c3f1f9d0faa2650f1b82de1b800
takuma0531 <tb.masaki0531@gmail.com>
2020-11-25 15:36:14 +0200

    create color table

--- app/src/main/java/com/example/customfortune/database/color/Color.kt
+++ app/src/main/java/com/example/customfortune/database/color/Color.kt
@@ -1,2 +1,15 @@
 package com.example.customfortune.database.color
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.customfortune.R
+
+@Entity(tableName = "color_table")
+data class Color(
+    @PrimaryKey(autoGenerate = true)
+    var colorId: Long = 0L,
+
+    @ColumnInfo(name = "color")
+    val color: Int = R.color.purple_500
+)
--- app/src/main/java/com/example/customfortune/database/color/ColorDao.kt
+++ app/src/main/java/com/example/customfortune/database/color/ColorDao.kt
@@ -1,2 +1,12 @@
 package com.example.customfortune.database.color
 
+import androidx.room.*
+
+@Dao
+interface ColorDao {
+    @Query("SELECT * from color_table WHERE colorId = :key")
+    suspend fun get(key: Long): Color?
+
+    @Update(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun update(color: Color)
+}
