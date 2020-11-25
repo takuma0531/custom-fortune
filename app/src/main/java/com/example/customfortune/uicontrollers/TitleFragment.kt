1a684630bc6c8c84c7cecab926764fe41aaced1d
takuma0531 <tb.masaki0531@gmail.com>
2020-11-25 15:00:39 +0200

    add clicklistener to start button

--- app/src/main/java/com/example/customfortune/uicontrollers/TitleFragment.kt
+++ app/src/main/java/com/example/customfortune/uicontrollers/TitleFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.example.customfortune.R
 import com.example.customfortune.databinding.FragmentTitleBinding
 
@@ -17,6 +18,10 @@ class TitleFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container,false)
+
+        binding.buttonStart.setOnClickListener { view: View ->
+            view.findNavController().navigate(R.id.action_titleFragment_to_resultFragment)
+        }
 
         return binding.root
     }
