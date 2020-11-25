403f1de77b9a9cfa0037ae3bcce811d627192617
takuma0531 <tb.masaki0531@gmail.com>
2020-11-25 15:48:26 +0200

    create card table

--- app/src/main/java/com/example/customfortune/database/card/Card.kt
+++ app/src/main/java/com/example/customfortune/database/card/Card.kt
@@ -1,2 +1,17 @@
 package com.example.customfortune.database.card
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "card_table")
+data class Card(
+    @PrimaryKey(autoGenerate = true)
+    var cardId: Long = 0L,
+
+    @ColumnInfo(name = "image")
+    val image: String,
+
+    @ColumnInfo(name = "description")
+    val description: String
+)
--- app/src/main/java/com/example/customfortune/database/card/CardDao.kt
+++ app/src/main/java/com/example/customfortune/database/card/CardDao.kt
@@ -1,2 +1,21 @@
 package com.example.customfortune.database.card
 
+import androidx.room.*
+
+@Dao
+interface CardDao {
+    @Query("SELECT * from card_table WHERE cardId = :key")
+    suspend fun get(key: Long): Card?
+
+    @Query("SELECT * from card_table")
+    suspend fun getAll(): List<Card>?
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(card: Card)
+
+    @Update(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun update(card: Card)
+
+    @Delete
+    suspend fun delete(card: Card)
+}
