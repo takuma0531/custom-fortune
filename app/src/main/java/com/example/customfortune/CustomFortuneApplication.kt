8fcbef91761f82aba9c512b5457e880ca2b6aa9f
takuma0531 <tb.masaki0531@gmail.com>
2020-11-26 13:56:15 +0200

    add coroutine scope in app

--- app/src/main/java/com/example/customfortune/CustomFortuneApplication.kt
+++ app/src/main/java/com/example/customfortune/CustomFortuneApplication.kt
@@ -5,9 +5,13 @@ import com.example.customfortune.database.FortuneDatabase
 import com.example.customfortune.repository.CardsRepository
 import com.example.customfortune.repository.ColorsRepository
 import com.example.customfortune.repository.UsersRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
 
 class CustomFortuneApplication : Application() {
-    val database by lazy { FortuneDatabase.getDatabase(this) }
+    private val applicationScope = CoroutineScope(SupervisorJob())
+
+    val database by lazy { FortuneDatabase.getDatabase(this, applicationScope) }
     val repository by lazy {
         CardsRepository(database.cardDao())
         ColorsRepository(database.colorDao())
