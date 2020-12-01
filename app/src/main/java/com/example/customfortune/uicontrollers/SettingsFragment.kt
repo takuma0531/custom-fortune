4203677c9e4ca328b4311b955de7fac610708fe0
takuma0531 <tb.masaki0531@gmail.com>
2020-12-01 13:47:46 +0200

    create layout of fragment settings

---
+++ app/src/main/java/com/example/customfortune/uicontrollers/SettingsFragment.kt
@@ -0,0 +1,23 @@
+package com.example.customfortune.uicontrollers
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.customfortune.R
+import com.example.customfortune.databinding.FragmentAboutBinding
+
+class SettingsFragment : Fragment() {
+    private lateinit var binding: FragmentAboutBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_settings, container, false)
+
+        return binding.root
+    }
+}
--- app/src/main/java/com/example/customfortune/viewmodels/CardViewModel.kt
+++ app/src/main/java/com/example/customfortune/viewmodels/CardViewModel.kt
@@ -6,17 +6,7 @@ import com.example.customfortune.repository.CardsRepository
 import kotlinx.coroutines.launch
 
 class CardViewModel(private val repository: CardsRepository): ViewModel() {
-    private val _cards: MutableLiveData<List<Card>> = MutableLiveData()
-    val cards: LiveData<List<Card>>
-        get() = _cards
-
-    init {
-        _cards.value = listOf(
-            Card("Image1", "desc1"),
-            Card("Image2", "desc2")
-        )
-//        repository.cards.asLiveData()
-    }
+    val cards: LiveData<List<Card>> = repository.cards.asLiveData()
 
     fun get(key: Long): LiveData<Card> =  repository.get(key).asLiveData()
 
