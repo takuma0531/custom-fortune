8225b1301859ad2b280dc054e295eeb238910b43
takuma0531 <tb.masaki0531@gmail.com>
2020-12-03 02:45:42 +0200

    create card button

--- app/src/main/java/com/example/customfortune/uicontrollers/CreateFortuneFragment.kt
+++ app/src/main/java/com/example/customfortune/uicontrollers/CreateFortuneFragment.kt
@@ -6,11 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import com.example.customfortune.MainActivity
 import com.example.customfortune.R
+import com.example.customfortune.database.card.Card
 import com.example.customfortune.databinding.FragmentCreateFortuneBinding
+import com.example.customfortune.utils.DependencyService
+import com.example.customfortune.viewmodels.CardViewModel
 
 class CreateFortuneFragment : Fragment() {
     private lateinit var binding: FragmentCreateFortuneBinding
+    private lateinit var viewModel: CardViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,6 +23,22 @@ class CreateFortuneFragment : Fragment() {
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_create_fortune, container, false)
 
+        setupViewModel()
+        clickCreateButton()
+
         return binding.root
+    }
+
+    private fun setupViewModel() {
+        viewModel = DependencyService.serveCardViewModel((activity as MainActivity?)!!)
+    }
+
+    private fun clickCreateButton() {
+        // TODO:
+        val image = "sample image"
+        val description = binding.textFortuneDescriptionTitle.editableText.toString()
+        val card = Card(description, image)
+
+        viewModel.insert(card)
     }
 }
